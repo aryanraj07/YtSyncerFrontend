99931f212c9db5b4fb642423360dbf9bfcb0fb2f
aryanraj07 <aryanraj010010@gmail.com>
2025-09-06 17:05:52 +0530

    made changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div>Copyright| Developer</div>;
 };
 
 export default Footer;
--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Header from "../components/header/Header";
 import { Outlet } from "react-router-dom";
-import Footer from "../components/Footer/Footer";
+
 import { AuthProvider } from "../context/AuthContext";
 import { SocketProvider } from "../context/SocketContext";
 import { ToastContainer } from "react-toastify";
-
+import Footer from "../components/footer/Footer";
 const MainLayouts = () => {
   return (
     <AuthProvider>
