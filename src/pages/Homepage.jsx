ec35b1c5bbc93024027ff534129f5edfdd743952
aryanraj07 <aryanraj010010@gmail.com>
2025-11-19 15:08:59 +0530

    made changes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,7 +5,7 @@ import "../styles/page/Home/HomePage.css";
 const Homepage = () => {
   return (
     <div className="hero-section flex items-center justify-center">
-      <div className="hero-content flex gap-4 md:gap-8  justify-center flex-col ">
+      <div className="hero-content flex gap-4 md:gap-8  justify-center flex-col flex-1 p-2">
         <h1 className="main-heading">
           {" "}
           Watch Youtube <br /> Together in Sync
@@ -14,12 +14,12 @@ const Homepage = () => {
           Create private rooms,invite friends and enjoy synced playback+chat in
           real time
         </p>
-        <div className="btn-container flex items-center  justify-center md:justify-start gap-4 w-100">
+        <div className="btn-container flex items-center  justify-center lg:justify-start gap-4 w-100">
           <button className="primary-btn">Create Room</button>
           <button className="outline-btn">Login/Signup</button>
         </div>
       </div>
-      <div className="hero-img ">
+      <div className="hero-img flex-1">
         <img src="/hero-image.png" alt="Hero Banner" loading="lazy" />
       </div>
     </div>
